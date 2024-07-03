4b4c222fe148c26322af6fb0bcfe2daf06fe5df4
Bibek Poudel <105065910+BibekPoudel855@users.noreply.github.com>
2024-07-03 21:06:34 +0545

    Add files via upload

---
+++ Project_7/script.js
@@ -0,0 +1,103 @@
+// source website for api key
+// https://newsapi.org/
+let apiKey = "d19db930781145a69e1d13dcbbb97cbe";
+let baseURL = "https://newsapi.org/v2/everything?q=";
+// Selection of elements
+let loader = document.querySelector("#loader");
+let paraMSG = document.querySelector("#para-MSG");
+let cardContainer = document.querySelector(".card-container");
+let aTagArray = document.querySelectorAll(".atag");
+let navButton = document.querySelector("#nav-button");
+let navInput = document.querySelector("#nav-search");
+let navLogo = document.querySelector(".nav-logo");
+//news you want to visible
+let categoryNews = "nepali";
+
+//event that will be fired when the page is loaded
+window.addEventListener("load", (e) => {
+  fetchNews(categoryNews);
+});
+async function fetchNews(query) {
+  loader.style.display = "flex";
+  try {
+    paraMSG.innerHTML = `Showing ${query.toUpperCase()} Related News`;
+    let response = await fetch(
+      `${baseURL}${query}&apiKey=${apiKey}&sortBy=publishedAt&apiKey=${apiKey}`
+    );
+    let data = await response.json();
+    bindData(data);
+  } catch (error) {
+    console.log("error occured", error);
+  } finally {
+    console.log("fetchNews function executed");
+    loader.style.display = "none";
+    cardContainer.style.display = "flex";
+  }
+}
+let clutter = ``;
+function bindData(data) {
+  articleArray = data.articles;
+  articleArray.forEach((article) => {
+    if (!article.urlToImage) {
+      return;
+    } else {
+      clutter += `<div class="card">
+            <div class="card-header">
+                <img src="${article.urlToImage}" alt="News_IMG" id="news-img">
+            </div>
+            <div class="card-content">
+                <h3 id="news-title" class="news-title"><a target="_blank" href="${article.url}">${article.title}</a></h3>
+                <h6 id="news-source" class="news-source">${article.source.name} ${article.publishedAt}</h6>
+                <p class="news-description" id="news-description">${article.description}</p>
+            </div>
+        </div>`;
+    }
+  });
+  cardContainer.innerHTML = clutter;
+}
+
+currSelected = document.querySelector("#nepali");
+currSelected.classList.add("active");
+function changeNewsCategoryByOnClickAnchorTag() {
+  aTagArray.forEach((aTag) => {
+    aTag.addEventListener("click", (e) => {
+      // to add active class on the clicked anchor tag
+      currSelected.classList.remove("active");
+      currSelected = e.target;
+      e.target.classList.add("active");
+      // to change category of news when click a tag
+      clutter = ``;
+      let query = e.target.getAttribute("id");
+      paraMSG.innerHTML = `Showing ${query} News`;
+      fetchNews(query);
+    });
+  });
+}
+changeNewsCategoryByOnClickAnchorTag();
+
+// search news by input field
+navButton.addEventListener("click", () => {
+  clutter = ``;
+  if (navInput.value == "") {
+    navInputValue = "nepali";
+    fetchNews(navInputValue);
+  } else {
+    let navInputValue = navInput.value;
+    fetchNews(navInputValue);
+    // to remove active class from the anchor tag
+    // when search button is clicked
+    currSelected.classList.remove("active");
+    currSelected = document.querySelector("#nepali");
+    currSelected.classList.add("active");
+  }
+});
+
+//reload page when click on the logo
+navLogo.addEventListener("click", () => {
+  window.location.reload();
+});
+
+
+
+
+
